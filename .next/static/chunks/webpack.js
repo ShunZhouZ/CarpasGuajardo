3dd30ac6597e67d94de7f810cf56a44e9ac940b8
ignvcio42 <ignacio.guajardo_m@mail.udp.cl>
2023-05-15 13:44:29 -0400

    cosas de next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a2b20fe56ee36966"; }
+/******/ 		__webpack_require__.h = function() { return "9c076948a6dd84d2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
