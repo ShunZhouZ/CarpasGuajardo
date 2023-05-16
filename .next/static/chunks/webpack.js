7dbe924a87f1c8e0ebc175eeb950100c7fa89a3e
ShunZhouZ <82847454+ShunZhouZ@users.noreply.github.com>
2023-05-15 21:18:03 -0400

    switches modificar evento arreglado

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b8b7efc2766accb5"; }
+/******/ 		__webpack_require__.h = function() { return "13c90d8d83df3a40"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
