be53cf2e9a528b647b4f7fec53c97a5b3a498247
bvlebvle <valentina.diaz_go@mail.udp.cl>
2023-05-30 20:21:10 -0400

    vdf

--- components/navbar.js
+++ components/navbar.js
@@ -6,69 +6,63 @@ import Image from "react-bootstrap/Image";
 import "react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css";
 import useUser from "../lib/useUser";
 
-
-
 import React from "react";
 
 //eliminar si no os gusta el image, linea 13 al 19 y agregar Carpas Guajardo
 const NavBar = () => {
-
 	const { user, mutateUser } = useUser({
-        redirectIfFound: true,
-    });
+		redirectIfFound: true
+	});
 
-	if(!user) return <div>Cargando...</div>
+	if (!user) return <div>Cargando...</div>;
 
 	return (
-		
 		<>
-		{user.isLoggedIn && (
-		<Navbar className="mb-4" bg="primary" variant="dark" expand="lg">
-			<Container>
-				<Navbar.Brand href="#home"> Carpas Guajardo</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="me-auto">
-						<Nav.Link href="#home">Inicio</Nav.Link>
-						<NavDropdown title="Eventos" id="basic-nav-dropdown" bg="primary" variant="dark">
-							<NavDropdown.Item href="/eventos/agendar-evento">Agendar</NavDropdown.Item>
-							<NavDropdown.Item href="/eventos/eliminar-evento">Lista de eventos</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="/eventos/visualizar-admin">Visualizar calendario</NavDropdown.Item>
-						</NavDropdown>
-						<NavDropdown title="Visitas técnicas" id="basic-nav-dropdown" bg="primary" variant="dark">
-							<NavDropdown.Item href="/visitas-tecnicas/agendar-visita">Agendar</NavDropdown.Item>
-							<NavDropdown.Item href="/visitas-tecnicas/eliminar-visita">Lista de visitas</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="/visitas-tecnicas/visualizar-visitas-admin">Visualizar calendario</NavDropdown.Item>
-						</NavDropdown>
-						<NavDropdown title="Trabajadores" id="basic-nav-dropdown" bg="primary" variant="dark">
-							<NavDropdown.Item href="/trabajadores/add-worker">Agregar</NavDropdown.Item>
-							<NavDropdown.Item href="/trabajadores/eliminar-trabajadores">Lista de trabajadores</NavDropdown.Item>
-
-						</NavDropdown>
-						<NavDropdown title="Contactos" id="basic-nav-dropdown" bg="primary" variant="dark">
-							<NavDropdown.Item href="#action/3.1">Agregar</NavDropdown.Item>
-							<NavDropdown.Item href="/contactos/eliminar-contacto">Lista de contactos</NavDropdown.Item>
-						</NavDropdown>
-						<NavDropdown title="Inventario" id="basic-nav-dropdown" bg="primary" variant="dark">
-							<NavDropdown.Item href="#action/3.1">Agregar</NavDropdown.Item>
-							<NavDropdown.Item href="/inventario/eliminar-inventario">Lista de inventario</NavDropdown.Item>
-						</NavDropdown>
-					</Nav>
-				</Navbar.Collapse>
-				<Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-					<NavDropdown title="ADMINISTRADOR" id="basic-nav-dropdown" bg="primary" variant="dark">
-						<NavDropdown.Item href="#action/3.1">Mi perfil</NavDropdown.Item>
-						<NavDropdown.Divider />
-						<NavDropdown.Item href="/api/logout">Cerrar sesión</NavDropdown.Item>
-					</NavDropdown>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-		)}
+			{user.isLoggedIn && (
+				<Navbar className="mb-4" bg="primary" variant="dark" expand="lg">
+					<Container>
+						<Navbar.Brand href="#home"> Carpas Guajardo</Navbar.Brand>
+						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+						<Navbar.Collapse id="basic-navbar-nav">
+							<Nav className="me-auto">
+								<Nav.Link href="/">Inicio</Nav.Link>
+								<NavDropdown title="Eventos" id="basic-nav-dropdown" bg="primary" variant="dark">
+									<NavDropdown.Item href="/eventos/agendar-evento">Agendar</NavDropdown.Item>
+									<NavDropdown.Item href="/eventos/eliminar-evento">Lista de eventos</NavDropdown.Item>
+									<NavDropdown.Divider />
+									<NavDropdown.Item href="/eventos/visualizar-admin">Visualizar calendario</NavDropdown.Item>
+								</NavDropdown>
+								<NavDropdown title="Visitas técnicas" id="basic-nav-dropdown" bg="primary" variant="dark">
+									<NavDropdown.Item href="/visitas-tecnicas/agendar-visita">Agendar</NavDropdown.Item>
+									<NavDropdown.Item href="/visitas-tecnicas/eliminar-visita">Lista de visitas</NavDropdown.Item>
+									<NavDropdown.Divider />
+									<NavDropdown.Item href="/visitas-tecnicas/visualizar-visitas-admin">Visualizar calendario</NavDropdown.Item>
+								</NavDropdown>
+								<NavDropdown title="Trabajadores" id="basic-nav-dropdown" bg="primary" variant="dark">
+									<NavDropdown.Item href="/trabajadores/add-worker">Agregar</NavDropdown.Item>
+									<NavDropdown.Item href="/trabajadores/eliminar-trabajadores">Lista de trabajadores</NavDropdown.Item>
+								</NavDropdown>
+								<NavDropdown title="Contactos" id="basic-nav-dropdown" bg="primary" variant="dark">
+									<NavDropdown.Item href="#action/3.1">Agregar</NavDropdown.Item>
+									<NavDropdown.Item href="/contactos/eliminar-contacto">Lista de contactos</NavDropdown.Item>
+								</NavDropdown>
+								<NavDropdown title="Inventario" id="basic-nav-dropdown" bg="primary" variant="dark">
+									<NavDropdown.Item href="#action/3.1">Agregar</NavDropdown.Item>
+									<NavDropdown.Item href="/inventario/eliminar-inventario">Lista de inventario</NavDropdown.Item>
+								</NavDropdown>
+							</Nav>
+						</Navbar.Collapse>
+						<Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+							<NavDropdown title="ADMINISTRADOR" id="basic-nav-dropdown" bg="primary" variant="dark">
+								<NavDropdown.Item href="#action/3.1">Mi perfil</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="/api/logout">Cerrar sesión</NavDropdown.Item>
+							</NavDropdown>
+						</Navbar.Collapse>
+					</Container>
+				</Navbar>
+			)}
 		</>
-		
 	);
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,53 @@
 import Head from "next/head";
 import withSession from "../lib/session";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Table } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
-export default function Home({ user }) {
+import moment from "moment";
+
+export default function Home({ user, allEvents }) {
+	// traer todos los eventos
+	const events = [];
+
+	for (let i = 0; i < allEvents.length; i++) {
+		const fecha_inicio = moment(allEvents[i].fecha_inicio).format("YYYY-MM-DD HH:mm:ss");
+		const fecha_termino = moment(allEvents[i].fecha_termino).format("YYYY-MM-DD HH:mm:ss");
+		events.push({
+			id: allEvents[i]._id,
+			title: allEvents[i].nombre_cliente,
+			start: fecha_inicio,
+			end: fecha_termino,
+			allDay: false,
+			//otras cosas
+			contacto: allEvents[i].numero_contacto_cliente,
+			direccion: allEvents[i].direccion_cliente,
+			monto_total: allEvents[i].monto_total,
+			anticipo: allEvents[i].anticipo,
+			carpa: allEvents[i].carpa, //nueva
+			toldo: allEvents[i].toldo, //nueva
+			calefaccion: allEvents[i].calefaccion, //nueva
+			iluminacion: allEvents[i].Iluminacion, //nueva
+			cubre_piso: allEvents[i].cubre_piso,
+			metros_cuadrados: allEvents[i].metros_cuadrados,
+			descripcion: allEvents[i].descripcion
+		});
+	}
+
+	const filtrarBusqueda = (fecha_hoy) => {
+		console.log(fecha_hoy);
+		var resultado = events.filter((elemento) => {
+			if (elemento.start.includes(fecha_hoy)) {
+				return elemento;
+			}
+		});
+		if (resultado.length > 0) {
+			return resultado;
+		} else {
+			return [];
+		}
+	};
+
+	const eventos_hoy = filtrarBusqueda(moment().format("YYYY-MM-DD"));
+
 	return (
 		<div>
 			<Head>
@@ -36,6 +81,35 @@ export default function Home({ user }) {
 					</div>
 				</Col>
 			</Row>
+
+			<Container className="text-center mt-4">
+				<Row className="justify-content-center">
+					<Col md={6}>
+						<h2>Eventos de hoy</h2>
+						<Table responsive striped bordered hover>
+							<thead>
+								<tr key={0}>
+									<th style={{ width: "15%" }}>Nombre</th>
+									<th style={{ width: "15%" }}>Contacto</th>
+									<th style={{ width: "15%" }}>Dirección</th>
+								</tr>
+							</thead>
+							<tbody>
+								{eventos_hoy.map((event, index) => (
+									<tr key={index}>
+										<td>{event.title}</td>
+										<td>{event.contacto}</td>
+										<td>{event.direccion}</td>
+									</tr>
+								))}
+							</tbody>
+						</Table>
+					</Col>
+					<Col md={6}>
+						<h2>Visitas de hoy</h2>
+					</Col>
+				</Row>
+			</Container>
 
 			<Container className="text-center mt-4">
 				<Row className="justify-content-center">
@@ -75,14 +149,21 @@ export default function Home({ user }) {
 export const getServerSideProps = withSession(async function ({ req, res }) {
 	const user = req.session.get("user");
 
+	let getAllEvents = await fetch("http://localhost:3000/api/eventos", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json"
+		}
+	});
+	let allEvents = await getAllEvents.json();
 	if (user === undefined) {
 		res.setHeader("location", "/login");
 		res.statusCode = 302;
 		res.end();
-		return { props: {} };
+		return { props: { allEvents: allEvents.data } };
 	}
 
 	return {
-		props: { user: req.session.get("user") }
+		props: { user: req.session.get("user"), allEvents: allEvents.data }
 	};
 });
